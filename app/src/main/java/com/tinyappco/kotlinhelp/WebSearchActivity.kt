fd5de70a24a988423f6e16f6c351dbb144f1d4dd
Andrew Muncey <a.muncey@chester.ac.uk>
2022-12-07 16:51:29 +0000

    move webview to fragment

--- app/src/main/java/com/tinyappco/kotlinhelp/WebSearchActivity.kt
+++ app/src/main/java/com/tinyappco/kotlinhelp/WebSearchActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.tinyappco.kotlinhelp.databinding.ActivityWebSearchBinding
 
@@ -19,8 +20,8 @@ class WebSearchActivity : AppCompatActivity() {
         val view = binding.root
 
         setContentView(view)
-        binding.webView.webViewClient = WebViewClient() //prevents opening in browser app
-        binding.webView.settings.javaScriptEnabled = true // required for search functionality on Kotlin and Android sites
+//        binding.webView.webViewClient = WebViewClient() //prevents opening in browser app
+//        binding.webView.settings.javaScriptEnabled = true // required for search functionality on Kotlin and Android sites
 
         var url = intent.getStringExtra("url")
 
@@ -28,14 +29,38 @@ class WebSearchActivity : AppCompatActivity() {
             url = savedInstanceState.getString("url")
         }
 
-        binding.webView.loadUrl(url!!) //should never be null
+//        binding.webView.loadUrl(url!!) //should never be null
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.webFragmentContainerView) as WebSearchFragment
+        fragment.loadUrl(url!!)
+
+
+
+        onBackPressedDispatcher.addCallback(this,object:OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                val intent = Intent()
+                //val fragment = supportFragmentManager.findFragmentById(R.id.webFragmentContainerView) as WebSearchFragment
+
+                intent.putExtra("url",fragment.getCurrentUrl())
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+
+        })
+
     }
 
-    override fun onBackPressed() {
-        val intent = Intent()
-        intent.putExtra("url",binding.webView.url)
-        setResult(Activity.RESULT_OK, intent)
-        finish()
-        super.onBackPressed()
-    }
+
+
+
+
+
+
+//    override fun onBackPressed() {
+//        val intent = Intent()
+//        intent.putExtra("url",binding.webView.url)
+//        setResult(Activity.RESULT_OK, intent)
+//        finish()
+//        super.onBackPressed()
+//    }
 }
---
+++ app/src/main/java/com/tinyappco/kotlinhelp/WebSearchFragment.kt
@@ -0,0 +1,55 @@
+package com.tinyappco.kotlinhelp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
+import com.tinyappco.kotlinhelp.databinding.FragmentWebSearchBinding
+
+
+class WebSearchFragment : Fragment() {
+
+    private lateinit var binding : FragmentWebSearchBinding
+    private var url: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentWebSearchBinding.inflate(inflater)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.webView.webViewClient = WebViewClient() //prevents opening in browser app
+        binding.webView.settings.javaScriptEnabled = true // required for search functionality on Kotlin and Android sites
+
+        if (url != null){
+            binding.webView.loadUrl(url!!)
+        }
+
+    }
+
+    fun loadUrl(url:String){
+        if (this::binding.isInitialized){
+            binding.webView.loadUrl(url)
+        }
+        this.url = url
+    }
+
+    fun getCurrentUrl() : String?{
+        return binding.webView.url
+    }
+
+
+}
